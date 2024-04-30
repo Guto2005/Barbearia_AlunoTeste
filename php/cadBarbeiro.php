7c8f592b477a8376f92dc09a45fc21a8deaeb89c
gustavo <gustavo@gpsbr.net>
2024-04-30 17:28:24 -0300

    git inicial

---
+++ php/cadBarbeiro.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST['nome'])&&isset($_POST['senha'])&& $_POST['email']&& ($_POST['telefone'])&& ($_POST['aniversario'])&& ($_POST['data'])){
+
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+$email = $_POST['email'];
+$cpf = $_POST['cpf'];
+$telefone = $_POST['telefone'];
+$aniversario = $_POST['aniversario'];
+$dataInicio = $_POST['data'];
+}
+
+?>
